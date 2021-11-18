8b899623652b8c29c673a8ac95e029d6ec5f84bf
dc3260 <dc3260@columbia.edu>
2021-11-18 15:27:53 -0500

    initial commit

---
+++ src/app/about-us/about-us.component.ts
@@ -0,0 +1,15 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-us',
+  template: `
+    <h2>About Us</h2>
+    <p>
+      We donate all profits of your orders to charity organizations that you designate!
+    </p>
+  `
+})
+export class AboutUsComponent {
+  // The code in this class drives the component's behavior.
+}
